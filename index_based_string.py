493c8996b3f702be7efe722faa3bc9ee420673cc
vegainfo <54527290-vegainfo@users.noreply.replit.com>
2026-02-13 16:48:29 +0000

    Initial commit

---
+++ index_based_string.py
@@ -0,0 +1,7 @@
+name = 'Kristine'
+age = 12
+product = 'Python eLearning Course'
+
+greeting = "Hi {0}, you are listed as {1} years old. You have purchased: {2}...".format(name, age, product)
+
+print(greeting)
---
+++ main.py
@@ -0,0 +1,8 @@
+name = 'Kristine'
+age = 12
+product = 'Python eLearning Course'
+from_account = 'Jordan'
+
+greeting = f"Product Purchase: {product} - Hi {name}, you are listed as {age} years old. - {from_account}"
+
+print(greeting)
