9bf9720095c88e9e59b72b54be452895421a55b4
vegajud <judit.vega.info@gmail.com>
2026-02-17 10:48:34 +0100

    Update checkpoint_3.py

--- checkpoint_3.py
+++ checkpoint_3.py
@@ -1,20 +1,30 @@
-text = "Hey there, this is a string." #1
+#Ejercicio 1
+text = "Hey there, this is a string." 
 number = 25
 hobbies = ["music", "books", "learn", "videogames"]
-temperature = 5
-is_cold = temperature < 15
-if is_cold:
-  print("Don't forget your scarf!")
-else: 
-  print("Enjoy the heat!")
-print (is_cold)
+boolean = True
 
-three_letters = text[0:3] #2
-first_element = hobbies[0] #3
-new_number = number + 10 #4
-last_element = hobbies[-1] #5
+#Ejercicio 2
+three_letters = text[0:3]
+
+#Ejercicio 3
+first_element = hobbies[0]
+
+#Ejercicio 4
+new_number = number + 10
+
+#Ejercicio 5
+last_element = hobbies[-1]
+
+#Ejercicio 6
 names = "harry,alex,susie,jared,gail,conner"
-names_list = names.split(",") #6
-upper_text = text[0:3].upper() + text [3:] #7
-print(f"I think you should know that my favorite number is {number}") #8
-print("hello world") #9
+names_list = names.split(",")
+
+#Ejercicio 7
+upper_text = text[0:3].upper() + text [3:]
+
+#Ejercicio 8
+print(f"I think you should know that my favorite number is {number}")
+
+#Ejercicio 9
+print("hello world")
